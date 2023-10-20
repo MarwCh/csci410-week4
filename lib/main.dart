af6b23883a973f57fe123e7b137e66be814ddabd
oussama_hatoum <ousama.hatoum@gmail.com>
2023-10-20 17:06:26 +0300

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,33 @@ class MyApp extends StatelessWidget {
           title: Text('Home Page'),
           centerTitle: true,
         ),
-       body: Center(
-         child: Text('Hello World',
-         style: TextStyle(
-             fontSize: 24,
-             color: Colors.deepPurple
-         )
-         ),
-       ),
+       body: Column(
+         children: <Widget>[
+           SizedBox(height: 16.0,),
+           MyTextWidget(text:'Text 1'),
+           SizedBox(height: 16.0,),
+           MyTextWidget(text: 'Text 2')
+         ],
+       )
       )
+    );
+  }
+}
+
+class MyTextWidget extends StatelessWidget {
+  final String text;
+
+  const MyTextWidget({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(text,
+      style: TextStyle(
+          fontSize: 24,
+          color: Colors.deepPurple
+      )
+      ),
     );
   }
 }
